82f81533f4a6441dca6f301ab5fae72d3291f3f0
Sasacoriror <turksacir@hotmail.com>
2024-08-06 18:00:04 +0200

    Growth rate has been added

--- src/main/resources/static/Auto.js
+++ src/main/resources/static/Auto.js
@@ -1,6 +1,7 @@
 function runAll(){
     stockPriceAPI();
     cashFlowAPIV2();
+    stockCagr();
 }
 
 
@@ -18,6 +19,26 @@ async function cashFlowAPIV2(){
         const data = await response.json();
         console.log(data);
         return latestCashFlows(data);
+         //Cagr(data);
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
+}
+
+async function CagrAPI(){
+    let stock = document.getElementById("stock").value;
+    const apiUrl = 'https://api.polygon.io/vX/reference/financials?ticker=' + stock + '&apiKey=Ix4tpJivedA1nWgzXSR8nQjJV1si8jbE';
+
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+        return Cagr(data);
     } catch (error) {
         console.error('Error:', error);
         return null;
@@ -61,6 +82,7 @@ function stockPrice(data){
 }
 
 function latestCashFlows(data) {
+
     const results = data.results;
     let freeFlow = 0
 
@@ -90,13 +112,54 @@ function latestCashFlows(data) {
     }
 }
 
-function Cagr(){
+function Cagr(data){
 
+    const results = data.results;
+    let freeCashFlow = 0;
+    let ultimate = 0;
+
+    // Iterate through the first 4 entries in the array
+    //for (let i = 0; i < 5 && i < results.length; i++) {
+    if (results.length > 4) {
+        const latestResult = results[4];
+        const cashFlowStatement = latestResult.financials.cash_flow_statement;
+
+        const netCashFlowFromOperatingActivities = cashFlowStatement.net_cash_flow_from_operating_activities
+            ? cashFlowStatement.net_cash_flow_from_operating_activities.value
+            : 0;
+
+        const netCashFlowFromInvestingActivities = cashFlowStatement.net_cash_flow_from_investing_activities
+            ? cashFlowStatement.net_cash_flow_from_investing_activities.value
+            : 0;
+        /*
+                console.log(`Latest Period:`);
+                console.log(`Net Cash Flow from Operating Activities: ${netCashFlowFromOperatingActivities}`);
+                console.log(`Net Cash Flow from Investing Activities: ${netCashFlowFromInvestingActivities}`);
+         */
+        freeFlow = parseFloat(netCashFlowFromOperatingActivities) - parseFloat(netCashFlowFromInvestingActivities);
+        console.log('Free cash flow IS: '+freeFlow);
+
+        return freeFlow;
+    } else {
+        console.log('No financial data available.');
+    }
+}
+
+async function stockCagr(){
+    let cash = await cashFlowAPIV2();
+    let cagr = await CagrAPI();
+
+    let growth = ((cagr - cash)/cash)*100;
+    console.log('The CAGR is: '+growth);
+    document.getElementById('Growth').innerText = growth;
 }
 
 async function test(){
 
-    //let cash = await
-    //let shares = await
+    let cash = await cashFlowAPIV2();
+    let cagr = await CagrAPI();
+
+    let sum = ((cagr - cash)/cash)*100;
+    console.log('The CAGR is: '+sum);
 
 }
